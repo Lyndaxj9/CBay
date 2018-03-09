d6b5e49aa9b3bef12565b2eea22055002260d66e
nathan blanchard <dev_nathanblanchard@gmail.com>
2018-03-08 20:32:40 -0500

    added register and login

--- cbay-frontend/src/app/components/logintemp/logintemp.component.ts
+++ cbay-frontend/src/app/components/logintemp/logintemp.component.ts
@@ -1,42 +1,56 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
 
 @Component({
-    selector: 'app-logintemp',
-    templateUrl: './logintemp.component.html',
-    styleUrls: ['./logintemp.component.css']
+  selector: 'app-logintemp',
+  templateUrl: './logintemp.component.html',
+  styleUrls: ['./logintemp.component.css']
 })
 export class LogintempComponent {
 
-  constructor(public http: HttpClient, public router: Router) { }
+  constructor(public http: HttpClient, public router: Router) {
+  }
 
-    model = new Client('', '', '');
-    submitted = false;
-    unauthenticated = false;
-    url = 'http://54.213.131.230:8089/CBay/rest/user/get/';
-    clientType = ['buyer', 'seller',
+  model = new Client('', '', '');
+  submitted = false;
+  server_error = false;
+  unauthenticated = false;
+  url = 'http://54.213.131.230:8089/CBay/rest/user/get/';
+  clientType = ['buyer', 'seller',
     'moderator'];
 
-    onSubmit() {
-        this.submitted = false;
-      this.get_user_data().then(response => {
+  onSubmit() {
+    this.submitted = false;
+    this.get_user_data().then(response => {
+      try {
         let id = response;
-        if(Number.isInteger(id)){
+        if (Number.isInteger(id)) {
           sessionStorage.setItem('userid', id);
-          this.router.navigateByUrl('/profile');
-        }else{
+          this.router.navigateByUrl('/profile').catch(error=>{
+            this.server_error = true;
+            console.log(error);
+          });
+        }
+        else {
           this.unauthenticated = true;
         }
-      });
-    }
+      }
+      catch (ex) {
+        this.unauthenticated = true;
+        console.log(ex);
+      }
+    });
+  }
 
-    // TODO: Remove this when we're done
-    get diagnostic() { return JSON.stringify(this.model); }
+  // TODO: Remove this when we're done
+  get diagnostic() {
+    return JSON.stringify(this.model);
+  }
 
-    reset() {
-        this.model = new Client('', '', '');
-    }
+  reset() {
+    this.model = new Client('', '', '');
+  }
 
   get_user_data(): Promise<any> {
     return this.http.get(this.url + this.model.userName + '/' + this.model.password + '/' + this.model.type)
@@ -46,10 +60,9 @@ export class LogintempComponent {
 
 export class Client {
 
-    constructor(
-    public userName: string,
-     public password: string,
-    public type: string
-    ) {  }
+  constructor(public userName: string,
+              public password: string,
+              public type: string) {
+  }
 
 }
