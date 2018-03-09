2934abfc270ac27cabeffba430356f1a03929879
Lynda Ademola <lyndaxj9@gmail.com>
2018-03-09 14:05:45 -0500

    redirect works properly from registration and my profile

--- cbay-frontend/src/app/components/profile/profile.component.ts
+++ cbay-frontend/src/app/components/profile/profile.component.ts
@@ -84,34 +84,7 @@ export class ProfileComponent implements OnInit {
         });
     }
 
-    ngOnInit() {
-        /*this.currentUser = parseInt(sessionStorage.getItem('userid'), 10);
-        this.clientModel = new Client(this.http);
-        this.sub = this.route.params
-            .subscribe(params => {
-            this.userId = +params['id'];
-        });
-
-        // TODO probably have to change to get username so other way
-        this.clientModel.get(this.userId)
-            .then(user_data => {
-
-            this.clientModel.set_all_values(user_data);
-            console.log(this.clientModel);
-        }).catch(error => {
-            console.log(error);
-        });
-        console.log(sessionStorage.getItem('usertype'));
-        if (sessionStorage.getItem('usertype') === 'Buyer') {
-            this.options = this.buyerOptions;
-        } else if (sessionStorage.getItem('usertype') === 'Seller') {
-            this.options = this.sellerOptions;
-        } else if (sessionStorage.getItem('usertype') === 'Moderator') {
-            this.options = this.modOptions;
-        } else if (sessionStorage.getItem('usertype') === 'Admin') {
-            this.options = this.adminOptions;
-        }*/
-    }
+    ngOnInit() { }
 
     get_user_info() {
         this.currentUser = parseInt(sessionStorage.getItem('userid'), 10);
--- cbay-frontend/src/app/shared/models/url.ts
+++ cbay-frontend/src/app/shared/models/url.ts
@@ -1,6 +1,6 @@
 export class Url {
-    // private urlbase = 'http://54.213.131.230:8089/CBay/rest';
-    private urlbase = 'http://localhost:8089/CBay_Project/rest';
+    private urlbase = 'http://54.213.131.230:8089/CBay/rest';
+    // private urlbase = 'http://localhost:8089/CBay_Project/rest';
 
     get_urlbase() {
         return this.urlbase;
