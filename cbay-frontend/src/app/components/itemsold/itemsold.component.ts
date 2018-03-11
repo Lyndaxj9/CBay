41ee5e86a1b741a6a00b4307302110170c21d102
nathan blanchard <dev_nathanblanchard@gmail.com>
2018-03-11 00:58:43 -0500

    added sellers items sold

--- cbay-frontend/src/app/components/itemsold/itemsold.component.ts
+++ cbay-frontend/src/app/components/itemsold/itemsold.component.ts
@@ -38,27 +38,27 @@ export class ItemsoldComponent implements OnInit {
     this.list = [];
     this.temp_list = [];
     this.is_item_display = true;
-    this.http.get<Post[]>(this.url.get_urlbase() + '/item/get/all').subscribe(res => {
+    const user_id = Number.parseInt(sessionStorage.getItem('userid'));
+    console.log(this.url.get_urlbase() + '/order/get/all/sellers/trans/' + user_id);
+    let size = 0;
+    this.http.get<any[]>(this.url.get_urlbase() + '/order/get/all/sellers/trans/' + user_id).subscribe(res => {
       console.log(res);
-      let size = res.length;
       let i = 0;
-      const user_id = Number.parseInt(sessionStorage.getItem('userid'));
-      console.log(user_id);
-
-      res.forEach(item => {
-        if(user_id===item.userId)
-        {
-          this.list.push(item);
-          console.log(item);
-        }
-
-        i = i + 1;
-        if(size <= i && this.list)
-        {
-          this.temp_list = this.list.slice(0, this.max_number_of_items_on_a_page);
-          this.last_page = Math.ceil(this.list.length / this.max_number_of_items_on_a_page);
-        }
+      size = res.length;
+      res.forEach(items => {
+        this.http.get<Post[]>(this.url.get_urlbase() + '/item/get/' + items.itemId).subscribe(response => {
+          console.log(response);
+          response.quanitity = res[i].quanitity;
+          i = i + 1;
+          this.list.push(response);
+          if(size <= i && this.list)
+          {
+            this.temp_list = this.list.slice(0, this.max_number_of_items_on_a_page);
+            this.last_page = Math.ceil(this.list.length / this.max_number_of_items_on_a_page);
+          }
+        });
       });
+
     });
 
   }
@@ -84,7 +84,7 @@ export class ItemsoldComponent implements OnInit {
 }
 
 interface Post {
-  description: string;
+  description?: string;
   id: number;
   itemName:string;
   price:number;
